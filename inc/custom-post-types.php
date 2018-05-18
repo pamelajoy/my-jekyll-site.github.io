90a6b84ae69481d69ea2b8e1f155dd55eb20820a
pamelajoy <pbuschbacher@gmail.com>
2018-05-18 17:25:37 -0500

    beginning of content organization

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -1,23 +1,44 @@
 <?php
 
-if ( ! function_exists('tq_theme_prefix_create_post_type') ) :
-  function tq_theme_prefix_create_post_type() {
+function tq_theme_prefix_create_post_type() {
 
-    // Custom Post Type - Testimonials
-    register_post_type( 'testimonials',
-      array(
-        'labels' => array(
-          'name' => __( 'Testimonials' ),
-          'singular_name' => __( 'Testimonial' )
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'rewrite' => array('slug' => 'testimonials'),
-        'menu_icon'	=> 'dashicons-testimonial',
-      )
-    );
+  // Custom Post Type - Testimonials
+  register_post_type( 'testimonials',
+    array(
+      'labels' => array(
+        'name' => __( 'Testimonials' ),
+        'singular_name' => __( 'Testimonial' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'testimonials'),
+      'menu_icon'	=> 'dashicons-testimonial',
+    )
+  );
+  // Custom Post Type - Products
+  register_post_type( 'products',
+    array(
+      'labels' => array(
+        'name' => __( 'Products' ),
+        'singular_name' => __( 'Product' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'products'),
+      'menu_icon' => 'dashicons-store',
+    )
+  );
 
-  }
-endif;
+  register_taxonomy(
+    'product-line',
+    'products',
+    array(
+        'label' => __( 'Product Lines' ),
+        'rewrite' => array( 'slug' => 'product-line' ),
+        'hierarchical' => true,
+    )
+  );
+
+}
 
 add_action( 'init', 'tq_theme_prefix_create_post_type' );
