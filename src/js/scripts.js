4080482aea088cbc9b06ffece61b33007d740d5e
pamelajoy <pbuschbacher@gmail.com>
2019-04-21 21:47:40 -0500

    start homepage and headline animation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,14 @@
 // primary javascript file
 jQuery(document).ready(function($) {
+});
+var tickerWrapper = $('.js-ticker');
+console.log(tickerWrapper);
 
+tickerWrapper.each(function(element) {
+  var tickerTextNode = $(this).find('h3');
+  var tickerText = tickerTextNode.text();
+  var times = 40;
+  for(var i=0; i < times; i++){
+    tickerTextNode.append(" " + tickerText);
+  }
 });
